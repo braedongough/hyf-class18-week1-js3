7047f04cab9f78f477f85e7cd4c50921c0c7d3ff
Braedon Gough <braedon@pleo.io>
2021-06-20 15:45:32 +0200

    Update lesson stuff

---
+++ astros.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body')
+
+// Astronauts in space
+
+fetch('http://api.open-notify.org/astros.json')
+    .then((res) => res.json())
+    .then((data) => {
+        renderSpaceTitle(data.number)
+        renderAstroNames(data.people)
+    })
+
+function renderSpaceTitle(numberOfAstros) {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `There are ${numberOfAstros} astronauts in space, they are:`
+    body.appendChild(h2)
+}
+
+function renderAstroNames(people) {
+    const ul = document.createElement('ul')
+
+    people.forEach((person) => {
+        const li = document.createElement('li')
+        li.innerHTML = person.name
+        ul.appendChild(li)
+    })
+
+    body.appendChild(ul)
+}
--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-const body = document.querySelector('body')
+// Dog fan website
 
-// Astronauts in space
+function getRandomDogImage() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then((res) => res.json())
+        .then((data) => {
+            const img = document.getElementById('random-dog')
 
-fetch('http://api.open-notify.org/astros.json')
-    .then((res) => res.json())
-    .then((data) => {
-        renderSpaceTitle(data.number)
-        renderAstroNames(data.people)
-    })
-
-function renderSpaceTitle(numberOfAstros) {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `There are ${numberOfAstros} astronauts in space, they are:`
-    body.appendChild(h2)
+            img.setAttribute('src', data.message)
+        })
 }
 
-function renderAstroNames(people) {
-    const ul = document.createElement('ul')
-
-    people.forEach((person) => {
-        const li = document.createElement('li')
-        li.innerHTML = person.name
-        ul.appendChild(li)
-    })
-
-    body.appendChild(ul)
+function renderRandomDogImageInterval(interval) {
+    getRandomDogImage()
+    setInterval(getRandomDogImage, interval)
 }
 
-// Astronauts in space
+function renderListOfBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then((res) => res.json())
+        .then((data) => {
+            const ul = document.createElement('ul')
+            document.body.appendChild(ul)
+
+            const breeds = Object.keys(data.message)
+
+            breeds.forEach((breed) => {
+                const li = document.createElement('li')
+                li.innerHTML = breed
+
+                ul.appendChild(li)
+            })
+        })
+}
+
+function renderDogBreedImage(breedName) {
+    fetch(`https://dog.ceo/api/breed/${breedName}/images/random`)
+        .then((res) => res.json())
+        .then((data) => {
+            const figure = document.createElement('figure')
+            const img = document.createElement('img')
+            img.setAttribute('src', data.message)
+            document.body.appendChild(figure)
+            figure.appendChild(img)
+
+            const figcaption = document.createElement('figcaption')
+            figcaption.innerHTML = breedName
+            figure.appendChild(figcaption)
+        })
+}
+
+renderDogBreedImage('cattledog')
+renderRandomDogImageInterval(2000)
+renderListOfBreeds()
